56dce80b9e0ac18e133268a06675b0443efb11b7
Sweetashne <mario2114@gmail.com>
2021-04-21 16:57:33 +0200

    Fixed project files

--- Design Patterns/Program.cs
+++ Design Patterns/Program.cs
@@ -6,36 +6,6 @@ namespace Design_Patterns
 	{
 		static void Main(string[] args)
 		{
-			var builder = new PizzaBuilder();
-			var director = new PizzaDirector(builder);
-			string input;
-
-			Console.WriteLine("Menu:");
-			Console.WriteLine("1. Hawaiian");
-			Console.WriteLine("2. Primavera");
-			Console.WriteLine("3. Margherita");
-			input = Console.ReadLine();
-			switch (int.Parse(input))
-			{
-				case 1:
-					director.MakePizza("Hawaiian");
-					break;
-				case 2:
-					director.MakePizza("Primavera");
-					break;
-				case 3:
-					director.MakePizza("Margherita");
-					break;
-				default:
-					break;
-
-			}
-			Pizza pizza = builder.GetPizza();
-			Console.WriteLine(pizza.Name + " " + pizza.Cost + "$");
-			foreach (string ingredient in pizza.Igredients)
-			{
-				Console.WriteLine(ingredient);
-			}
 		}
 	}
 }
